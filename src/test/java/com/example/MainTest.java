4de9b610864056e17cb0842dcc5ff9e0a4a2288d
Lorene Uy <lorene.uy@gcash.com>
2025-09-17 11:54:36 +0800

    [TST] addition test cases

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -3,6 +3,7 @@ package com.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
@@ -19,4 +20,66 @@ public class MainTest {
         assertTrue(c.isOff());
     }
 
+    @Test
+    void isOn() {
+        c.setOn(true);
+        assertFalse(c.isOff());
+    }
+
+    @Test
+    void setInputA() {
+        int input = 1;
+        c.setInputA(input);
+
+        assertEquals(input, c.getInputA());
+    }
+
+    @Test
+    void setInputB() {
+        int input = 1;
+        c.setInputB(input);
+
+        assertEquals(input, c.getInputB());
+    }
+
+    @Test
+    void addPositiveNumbers() {
+        c.setInputA(1);
+        c.setInputB(2);
+
+        assertEquals(3, c.add());
+    }
+
+    @Test
+    void addNegativeNumbers() {
+        c.setInputA(-1);
+        c.setInputB(-2);
+
+        assertEquals(-3, c.add());
+    }
+
+    @Test
+    void addZero() {
+        c.setInputA(0);
+        c.setInputB(0);
+
+        assertEquals(0, c.add());
+
+        c.setInputB(1);
+
+        assertEquals(1, c.add());
+    }
+
+    @Test
+    void addPositiveAndNegativeNumbers() {
+        c.setInputA(1);
+        c.setInputB(-1);
+
+        assertEquals(0, c.add());
+
+        c.setInputA(-5);
+        c.setInputB(3);
+
+        assertEquals(-2, c.add());
+    }
 }
